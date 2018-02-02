f814143f489275a237c113d0ce3faed3871fd9ce
Eric Ryan Harrison <eric@moonrise.works>
2018-02-01 19:28:36 -0600

    Change back to analogWrite

--- art.ino
+++ art.ino
@@ -71,7 +71,7 @@ void loop() {
 	}
 	if ( val2A && val2B ) {
 		Serial.println('Light 2 on');
-		digitalWrite(out2, HIGH);
+		analogWrite(out2, 255);
 	}
 	if ( val3A && val3B ) {
 		analogWrite(out3, 255);
